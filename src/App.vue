d71e35676b166643b1b5e9fa1f5102b6cfa36534
Dylan <klj40702@gmail.com>
2024-07-11 16:10:41 +0800

    vee validate

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ import VeeValidate from './components/VeeValidate.vue'
 
 <template>
   <!-- <VCalendar /> -->
-  <Test />
-  <!-- <VeeValidate /> -->
+  <!-- <Test /> -->
+  <VeeValidate />
 </template>
 
 <style scoped></style>
--- src/components/VeeValidate.vue
+++ src/components/VeeValidate.vue
@@ -9,7 +9,7 @@
       <input v-model="password" v-bind="passwordAttrs" type="password" />
     </div>
     <hr />
-    <button>Sign up for newsletter</button>
+    <button :disabled="isDisabled">Sign up for newsletter</button>
     <hr />
     <pre>meta: {{ meta }}</pre>
     <hr />
@@ -17,52 +17,66 @@
     <hr />
     <pre>errors: {{ errors }}</pre>
     <hr />
-    <div>
-      Error:
-      <p v-show="errorMsg">{{ errorMsg }}</p>
-    </div>
   </form>
 </template>
 
 <script setup lang="ts">
 import { ref, watchEffect, computed } from 'vue'
-import { useForm, defineRule, configure } from 'vee-validate'
+import { useForm, defineRule, configure, type LazyComponentBindsConfig } from 'vee-validate'
 import { object as yupObject, string } from 'yup'
 import { toTypedSchema } from '@vee-validate/yup'
 
-configure({
-  validateOnInput: false,
-  validateOnChange: false,
-  validateOnBlur: false,
-  validateOnModelUpdate: false,
-})
-
-const { values, errors, meta, defineField, handleSubmit } = useForm({
+const { values, errors, meta, defineField, handleSubmit, submitCount } = useForm({
   validationSchema: toTypedSchema(
     yupObject({
-      email: string(),
-      password: string().when('email', ([email], schema) => {
-        console.log('== email value ==', email)
-
-        return email ? schema.optional() : schema.required('必須填一個')
-      }),
+      email: string().required().email(),
+      password: string().required().min(8),
     })
-    // .test('at-least-one', '至少需要填寫 A 或 B 其中一個', (value, ...args) => {
-    //   console.log('== args ==', args)
-    //   const { email, password } = value
-
-    //   return !!email || !!password
-    // })
   ),
 })
 
-const [email, emailAttrs] = defineField('email')
-const [password, passwordAttrs] = defineField('password')
+type T1 = ReturnType<LazyComponentBindsConfig>
 
-const errorMsg = computed(() => Object.values(errors.value)[0])
+function createNFormItemConfig(customAttrs: Record<string, unknown>, inputConfig?: () => T1 | Partial<T1>): LazyComponentBindsConfig {
+  return (state) => {
+    const _inputConfig = typeof inputConfig === 'function' ? inputConfig() : inputConfig
+
+    return {
+      model: 'modelValue',
+      props: {
+        validationStatus: state.errors[0] ? 'error' : undefined,
+        feedback: state.errors[0],
+        debounce: 500,
+        ...customAttrs,
+      },
+      ..._inputConfig,
+    }
+  }
+}
+
+function testConfig() {
+  return () => {
+    if (submitCount.value > 0) {
+      return {
+        validateOnBlur: true,
+        validateOnModelUpdate: true,
+      }
+    }
+
+    return {
+      validateOnBlur: true,
+      validateOnModelUpdate: false,
+    }
+  }
+}
+
+const [email, emailAttrs] = defineField('email', createNFormItemConfig({}, testConfig()))
+const [password, passwordAttrs] = defineField('password', createNFormItemConfig({}, testConfig()))
+
+const isDisabled = computed(() => Object.values(values).some((v) => !v))
 
 watchEffect(() => {
-  console.log('== errors ==', errors)
+  // console.log('== controlledValues ==', controlledValues.value, values)
 })
 
 const onSubmit = handleSubmit((values) => {
