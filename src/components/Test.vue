9f099d8e0677abeed56d51e6e20d5c18bda28332
Dylan <klj40702@gmail.com>
2024-10-13 14:59:39 +0800

    feat: SpeechRecognition api

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,41 +1,11 @@
 <template>
-  <h1>{{ obj1 }}</h1>
-  <h1>{{ obj2 }}</h1>
-  <h1>{{ obj3 }}</h1>
-  <h1>{{ obj4 }}</h1>
-  <button @click="onClick">Click</button>
+  <SpeechRecognition />
 </template>
 
 <script setup lang="ts">
 import { ref, reactive, computed, watch } from 'vue'
-import TestChild from './TestChild.vue'
 
-const obj1 = ref({ a: 1, b: 2, c: [] as number[] })
-const obj2 = reactive({ a: 1, b: 2, c: [] as number[] })
-const obj3 = computed(() => ({ a: obj1.value.a }))
-const obj4 = computed(() => ({ a: obj2.a }))
-
-watch(
-  obj1,
-  (newVal, oldVal) => {
-    console.log('obj1 changed', newVal, oldVal)
-  },
-  { deep: true }
-)
-watch(obj2, (newVal, oldVal) => {
-  console.log('obj2 changed', newVal, oldVal)
-})
-watch(obj3, (newVal, oldVal) => {
-  console.log('obj3 changed', newVal, oldVal)
-})
-watch(obj4, (newVal, oldVal) => {
-  console.log('obj4 changed', newVal, oldVal)
-})
-
-function onClick() {
-  obj1.value.a = 2
-  obj2.a = 2
-}
+import SpeechRecognition from './js-api-demo/SpeechRecognition.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/js-api-demo/SpeechRecognition.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)()
+
+recognition.lang = 'zh-TW'
+
+// recognition.continuous = true
+// recognition.interimResults = true
+
+recognition.onresult = (event) => {
+  const { transcript } = event.results[0][0]
+
+  console.log('結果:', transcript)
+}
+
+// recognition.onresult = (event) => {
+//   let finalTranscript = ''
+//   let interimTranscript = ''
+//   const result = event.results[0]
+//   const { isFinal } = result
+//   const [{ transcript }] = result
+
+//   if (isFinal) {
+//     finalTranscript = transcript
+//   } else {
+//     interimTranscript = transcript
+//   }
+
+//   console.log('即時結果:', interimTranscript)
+//   console.log('%c最終結果:', 'color:#EE66A6;', finalTranscript)
+// }
+
+// recognition.onspeechstart = () => {
+//   console.log('偵測到語音')
+// }
+
+// recognition.onspeechend = () => {
+//   console.log('語音結束')
+// }
+
+// recognition.onend = () => {
+//   console.log('語音識別結束')
+// }
+
+// recognition.onerror = (event) => {
+//   console.error('識別失敗:', event.error)
+// }
+
+// recognition.onnomatch = () => {
+//   console.log('無法匹配到語音')
+// }
+
+function start() {
+  recognition.start()
+}
+
+function stop() {
+  recognition.stop()
+}
+</script>
