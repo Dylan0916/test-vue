716be255e09f521e04c4599491eadef11cbed6b1
Dylan <klj40702@gmail.com>
2024-10-13 20:43:34 +0800

    feat: crypto api

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- <SpeechRecognition /> -->
   <!-- <SpeechSynthesisUtterance /> -->
-  <ScreenCapture />
+  <!-- <ScreenCapture /> -->
+  <Cryptography />
 </template>
 
 <script setup lang="ts">
@@ -10,6 +11,7 @@ import { ref, reactive, computed, watch } from 'vue'
 import SpeechRecognition from './js-api-demo/SpeechRecognition.vue'
 import SpeechSynthesisUtterance from './js-api-demo/SpeechSynthesisUtterance.vue'
 import ScreenCapture from './js-api-demo/ScreenCapture.vue'
+import Cryptography from './js-api-demo/Cryptography.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/js-api-demo/Cryptography.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <br />
+    <button @click="generateAESKey">gen AES key</button>
+    <button @click="encryptText">encrypt text</button>
+    <button @click="decryptText">decrypt text</button>
+    <hr />
+    <CryptographyRSA />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import CryptographyRSA from './CryptographyRSA.vue'
+
+let aesKey: CryptoKey | null = null
+let iv: Uint8Array | null = null
+let encryptedData: ArrayBuffer | null = null
+
+const text = ref('')
+
+function generateRandomValues() {
+  const array = new Uint8Array(12)
+  window.crypto.getRandomValues(array)
+  return array
+}
+
+async function generateAESKey() {
+  const key = await crypto.subtle.generateKey(
+    {
+      name: 'AES-GCM',
+      length: 256, // 128, 192 或 256
+    },
+    true,
+    ['encrypt', 'decrypt']
+  )
+
+  console.log('Generated AES Key:', key)
+
+  return key
+}
+
+async function decryptData(key: CryptoKey, data: BufferSource) {
+  if (!iv) {
+    return
+  }
+
+  const decrypted = await crypto.subtle.decrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+    },
+    key,
+    data
+  )
+
+  console.log('解密後的資料:', new TextDecoder().decode(decrypted))
+
+  return decrypted
+}
+
+async function encryptData(key: CryptoKey, data: BufferSource) {
+  const iv = generateRandomValues()
+  const encrypted = await crypto.subtle.encrypt(
+    {
+      name: 'AES-GCM',
+      iv,
+    },
+    key,
+    data
+  )
+
+  console.log('iv:', iv)
+  console.log('加密後的資料:', new Uint8Array(encrypted))
+
+  return { encrypted, iv }
+}
+
+async function encryptText() {
+  aesKey = await generateAESKey()
+
+  const data = new TextEncoder().encode(text.value)
+  const result = await encryptData(aesKey, data)
+
+  ;[iv, encryptedData] = [result.iv, result.encrypted]
+}
+
+async function decryptText() {
+  if (!aesKey || !encryptedData) {
+    return
+  }
+
+  await decryptData(aesKey, encryptedData)
+}
+</script>
