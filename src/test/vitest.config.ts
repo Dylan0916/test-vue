78b0bf642bed269c9cc1bedc7c76fc02e6373e02
Dylan <klj40702@gmail.com>
2025-06-08 12:03:28 +0800

    test

--- src/test/vitest.config.ts
+++ src/test/vitest.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      reporter: ['text'],
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
+    },
   },
 })
