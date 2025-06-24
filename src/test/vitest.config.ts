093122224f6afc62fc6e93836d09d62c78d4f267
Dylan <klj40702@gmail.com>
2025-06-24 16:30:18 +0800

    coverage

--- src/test/vitest.config.ts
+++ src/test/vitest.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    watch: false,
     globals: true,
     coverage: {
-      reporter: ['text'],
+      include: ['src/**'],
       thresholds: {
         statements: 70,
         branches: 60, // branches 通常最低，因為難測
