1b61c1834693d2c02fe2351c05d91d3639b58feb
Dylan <klj40702@gmail.com>
2025-11-03 16:13:27 +0800

    feat: enhance App.vue and IconList.vue with new features and styling
    - Updated App.vue to include NMessageProvider and theme overrides for improved styling.
    - Refactored IconList.vue to utilize a constant for icon names and added click functionality to copy icon names to clipboard with a success message.
    - Removed unused styles from style.css to streamline the layout.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,43 @@
+<template>
+  <NMessageProvider :theme-overrides="themeOverrides">
+    <NGlobalStyle />
+    <!-- <VCalendar /> -->
+    <!-- <Test /> -->
+    <!-- <Vueuse /> -->
+    <!-- <VeeValidate /> -->
+    <!-- <Proxy /> -->
+    <!-- <WebApi /> -->
+    <!-- <Vuetify /> -->
+    <!-- <Copper /> -->
+    <!-- <BulletedEditor /> -->
+    <!-- <LargeList /> -->
+    <IconList />
+  </NMessageProvider>
+</template>
+
 <script setup lang="ts">
+import { NConfigProvider, NMessageProvider, NGlobalStyle, type GlobalThemeOverrides } from 'naive-ui'
+
 import VCalendar from './components/VCalendar.vue'
 import Test from './components/Test.vue'
 import VeeValidate from './components/VeeValidate.vue'
 import Proxy from './components/Proxy.vue'
-import WebApi from './components/WebApi.vue'
 import Vueuse from './components/Vueuse.vue'
 import Vuetify from './components/Vuetify.vue'
 import Copper from './components/Copper.vue'
 import BulletedEditor from './components/BulletedEditor.vue'
 import LargeList from './components/large-list/LargeList.vue'
 import IconList from './components/IconList.vue'
-</script>
 
-<template>
-  <!-- <VCalendar /> -->
-  <!-- <Test /> -->
-  <!-- <Vueuse /> -->
-  <!-- <VeeValidate /> -->
-  <!-- <Proxy /> -->
-  <!-- <WebApi /> -->
-  <!-- <Vuetify /> -->
-  <!-- <Copper /> -->
-  <!-- <BulletedEditor /> -->
-  <!-- <LargeList /> -->
-  <IconList />
-</template>
+const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    fontSize: '36',
+    fontSizeMedium: '18',
+    fontSizeSmall: '16',
+    fontSizeTiny: '12',
+    fontSizeLarge: '20',
+  },
+}
+</script>
 
 <style scoped lang="scss"></style>
--- src/components/IconList.vue
+++ src/components/IconList.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="flex flex-wrap gap-4 gap-y-5">
-    <div v-for="iconName in iconNames" :key="iconName" class="text-red-400 flex flex-col items-center w-17 overflow-hidden">
+    <div
+      v-for="iconName in ICON_NAMES"
+      :key="iconName"
+      class="text-red-400 flex flex-col items-center w-17 cursor-pointer overflow-hidden"
+      @click="onIconClick(iconName)"
+    >
       <FunNowSvgIcon class="size-8" :name="iconName" />
-      <p class="m-0 mt-2 truncate w-full text-center">{{ iconName }}</p>
+      <p class="m-0 mt-2 truncate w-full text-center text-base">{{ iconName }}</p>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useClipboard } from '@vueuse/core'
+import { useMessage } from 'naive-ui'
+
+import { ICON_NAMES } from '@/constants/icon'
 import FunNowSvgIcon from './FunNowSvgIcon.vue'
 
-const iconMap = import.meta.glob('@/assets/icons/*.svg', { eager: true })
-const iconNames = Object.keys(iconMap).map(key => key.replace('/src/assets/icons/', '').replace('.svg', ''))
+const { copy } = useClipboard()
+const message = useMessage()
+
+function onIconClick(iconName: string) {
+  copy(iconName)
+  message.success(`${iconName} copied`)
+}
 </script>
