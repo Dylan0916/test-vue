8027ccd0c444014a2bf53433da6832d26f6134bb
Dylan <klj40702@gmail.com>
2026-01-13 16:27:18 +0800

    feat: naive-ui date-picker

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NDatePicker v-model:value="timestamp" type="date" />
+    <NDatePicker v-model:value="timestamp" :default-value="1767052800000" type="date" />
+    <NDatePicker :default-value="1767052800000" type="date" />
     <NDatePicker v-model:formatted-value="formattedValue" value-format="yyyy--MM--dd" format="yyyy/MM/dd" type="date" />
     <p>{{ timestamp }} , {{ formattedValue }}</p>
   </div>
