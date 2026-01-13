566ae93d0fca60737ca9d3ec72c30abecc7ad6ea
Dylan <klj40702@gmail.com>
2026-01-13 10:38:27 +0800

    test

---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <n-drawer v-bind="drawerProps">
+    <n-drawer-content v-bind="drawerContentProps">
+      <slot />
+    </n-drawer-content>
+  </n-drawer>
+</template>
+
+<script setup lang="ts">
+import { useAttrs, computed } from 'vue'
+
+import type { DrawerProps, DrawerContentProps } from 'naive-ui'
+import { NDrawer, NDrawerContent } from 'naive-ui'
+
+interface Props extends /* @vue-ignore */ DrawerProps {
+  drawerContentProps?: Partial<DrawerContentProps>
+}
+
+const props = defineProps<Props>()
+
+const attrs = useAttrs()
+
+const drawerProps = computed(() => ({ ...props, ...attrs }))
+</script>
--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,24 +1,20 @@
 <template>
   <div>
-    <header class="contents">
-      <p class="h-10 sticky top-0">1</p>
-      <p class="h-10">2</p>
-      <p class="h-10 sticky top-10">3</p>
-      <p class="h-10">4</p>
-      <p class="h-10 sticky top-20">5</p>
-      <p class="h-10">6</p>
-    </header>
-    <div class="border border-red-500 h-300">content</div>
+    <button @click="onClick">onClick</button>
+    <AppDrawer v-model:show="show" class="my-drawer">
+      <div>content</div>
+    </AppDrawer>
   </div>
 </template>
 
 <script lang="ts" setup>
-import axios from 'axios'
-import { ref, watch } from 'vue'
-import { useAsyncState } from '@vueuse/core'
-import { get } from 'lodash-es'
+import { ref } from 'vue'
+
+import AppDrawer from '@/components/AppDrawer.vue'
+
+const show = ref(false)
 
 function onClick() {
-  console.log(get({ a: 1 }, 'a'))
+  show.value = true
 }
 </script>
